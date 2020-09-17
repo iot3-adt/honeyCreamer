39a17bb687aa97993075b0f206dffe20f1c677b7
Ivanych <zakaz@oldprinters.ru>
2020-09-17 20:47:33 +0300

    Начало класса индикации

---
+++ src/Vild.h
@@ -0,0 +1,16 @@
+#ifndef VILD_H
+#define VILD_H
+
+#include <Wire.h> 
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+class Vild {
+
+  public:
+    Vild();
+    void outStr(int , String);  //входные параметры: номер строки индикатора, строка
+};
+
+
+#endif
