4512e55359281c655150663425e0cefa1693383a
Karatonov Kirill <65410613+Corvax47@users.noreply.github.com>
2020-10-02 12:24:04 +0300

    Чуть-чуть поправил temperatura.cpp

--- src/Temperatura.cpp
+++ src/Temperatura.cpp
@@ -3,10 +3,10 @@
 Temperatura::Temperatura(uint8_t pin, char deviceAddress) {
 	OneWire oneWire(pin);                                       // вход датчиков 18b20, аналоговый А1, он же 15 цифровой 
 	DallasTemperature ds(&oneWire);
+	ds.begin();
 }
 //-------------------------------------
-float Temperatura::getTemperatura(){
-	ds.begin();
+float Temperatura::getTemperatura() {
 	float tempC = ds.getTempC(deviceAddress)
 	return tempC;
 }
